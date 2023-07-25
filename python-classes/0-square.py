c02b25714e1e9c0b06b1212a8488e9761bc84a1b
Ialbertine <a.ingabire1@alustudent.com>
2023-07-25 11:49:29 -0700

    0. My first square

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
-Square = __import__('0-square').Square
+"""
+This is a python file that
+has a class
+"""
 
-my_square = Square()
-print(type(my_square))
-print(my_square.__dict__)
+
+class Square:
+    """
+    This is an empty class square
+    """
+    pass
