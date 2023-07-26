4488b97b7cd0f8121e6a8cc0a291fa5569fb4119
Ialbertine <a.ingabire1@alustudent.com>
2023-07-26 05:17:47 -0700

    0. Simple rectangle

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""
+i'm going to create an empty class of rectangle that define rectangle
+""" 
+
+class rectangle: 
+"""
+Here's an empty class rectangle that define a rectangle
+"""
+pass
