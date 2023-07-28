de6f0cd7bccdc123b0ea7444334b8816a68b58c4
Ialbertine <a.ingabire1@alustudent.com>
2023-07-28 04:44:09 -0700

    task 101

--- python-inheritance/101-add_attribute.py
+++ python-inheritance/101-add_attribute.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
-def add_attribute(obj, attribute, value):
-    if not hasattr(obj, '__dict__'):
+"""How far with comments """
+
+
+def add_attribute(obj, name, value):
+    """are you documented"""
+    if hasattr(obj, '__dict__') is False:
         raise TypeError("can't add new attribute")
-    setattr(obj, attribute, value)
+
+    setattr(obj, name, value)
