fbfcf1aa923bca42352ede82c18d8378edf2a76a
Ialbertine <a.ingabire1@alustudent.com>
2023-07-28 04:41:44 -0700

    task 100

--- python-inheritance/100-my_int.py
+++ python-inheritance/100-my_int.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
+"""Rebel int"""
+
+
 class MyInt(int):
+    """Rebel int class"""
+
     def __eq__(self, other):
-        """Override the equality operator (==)"""
+        """not equal when =="""
         return super().__ne__(other)
 
     def __ne__(self, other):
-        """Override the not equal operator (!=)"""
+        """equal when !="""
         return super().__eq__(other)
