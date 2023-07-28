501ebc9e07b59c06e61c7ecc6e80448ceb3a0e07
Ialbertine <a.ingabire1@alustudent.com>
2023-07-28 03:55:05 -0700

    task 1

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,10 +1,30 @@
 #!/usr/bin/python3
-"""define class MyList"""
-
 
 class MyList(list):
-    """class that inherits from list
-    with public instance method to print sorted list"""
+    """This is my class
+    MyList that inherits from
+    the built in class list
+    an creates a sorted function
+    """
+    # An initialization method of an empty list is done by the parent class
+    def _init_(self):
+        super()._init_()
 
-    def print_sorted(self):
-        print(sorted(self))
+    def append(self, value):
+        super().append(value)
+
+    def print_sorted(self, do_print=True):
+        """This is the sorted function
+        that sorts the the the instance
+        of the list mylist which inherits
+        the abilities of the class list
+        and the method .sort() and sorts
+        the instance not in place
+        """
+        sorted_list = sorted(self)
+        if do_print:
+            print(sorted_list)
+        return sorted_list
+
+    def _str_(self):
+        return super()._str_()
