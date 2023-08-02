9d7c28377c673b8cfdfbc0f422c1863e03ec8e51
Ialbertine <a.ingabire1@alustudent.com>
2023-08-01 17:40:53 -0700

    13. Number of shows by genre

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -3,6 +3,6 @@
 -- Don’t display a genre that doesn’t have any shows linked.
 --Records are ordered by descending number of shows linked.
 SELECT g.name AS genre, COUNT(*) AS number_of_shows 
-    FROM tv_genres AS g INNER JOIN tv_show_genres AS t 
-    ON g.id = t.genre_id GROUP BY g.name 
-    ORDER BY `number_of_shows` DESC;
+  FROM tv_genres AS g INNER JOIN tv_show_genres AS t  ON g.id = t.genre_id
+  GROUP BY g.name 
+  ORDER BY number_of_shows DESC;
