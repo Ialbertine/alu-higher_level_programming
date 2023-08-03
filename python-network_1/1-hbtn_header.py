c544eb392e070e5875f92284ec930b44e6bc8182
Ialbertine <a.ingabire1@alustudent.com>
2023-08-03 10:17:27 +0000

    1. Response header value #0

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Python script that takes in a URL, sends a request to the URL and displays the X-Request-Id header variable of a request to a given URL.
+"""displays the X-Request-Id header variable of a request to a given URL.
 
 Usage: ./1-hbtn_header.py <URL>
 """
