65005ed0466bc07d7c4943c9b7ad33e2fc912d14
Ialbertine <a.ingabire1@alustudent.com>
2023-09-27 08:27:48 -0700

    javascript tasks

---
+++ javascript-warm_up/2-arguments.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const agrs = process.argv.times(2);
+const num_Args = agrs.length;
+
+if (num_Args === 0){
+	console.log('No argument');
+} else if (num_Args == 1) {
+	console.log('Argument found');
+} else {
+	console.log('Argument found');
+}
+
